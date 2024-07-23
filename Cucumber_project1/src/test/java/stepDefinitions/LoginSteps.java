5d247b3d8e96902089b8559b3d24514ca55ed06e
Irs <irs@gmail.com>
2024-07-23 17:27:28 +0530

    Login Steps updated

--- Cucumber_project1/src/test/java/stepDefinitions/LoginSteps.java
+++ Cucumber_project1/src/test/java/stepDefinitions/LoginSteps.java
@@ -14,7 +14,7 @@ public class LoginSteps {
 	@When("user enters username and password") 
 	public void user_enters_username_and_password() 
 	{ // Write code here that turns the phrase above into concrete actions 
-		System.out.println("user enters username and password"); 
+		System.out.println("user enters username and password in Cap"); 
 		} 
 	@And("clicks on login button") 
 	public void clicks_on_login_button() 
